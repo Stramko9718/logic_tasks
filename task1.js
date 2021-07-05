7ddf5cce016a2b85d3deecf849a832802e03ef2b
Stramko9718 <81420220+Stramko9718@users.noreply.github.com>
2021-07-05 14:59:25 +0300

    fixed

--- task1.js
+++ task1.js
@@ -1,30 +1,39 @@
 "use strict"
 function maxAndMin(arr){
-        let obj = {
-            max: 0,
-            min: 0
-        }
-        let count_max = 0;
-        let count_min = 0;
-        for(let i=0; i<arr.length; i++){
-        for( let j = 0; j<arr.length; j++){
+    let obj = {
+        max: 0,
+        min: 0
+    }
+    let count_max = 0;
+    let count_min = 0;
+
+    for(let i=0; i<arr.length; i++){
+
+        for(let j = 0; j<arr.length; j++){
+
             if(arr[i] >= arr[j]){
             count_max ++;
             }
+
             if(arr[i] <= arr[j]){
                 count_min++
             }
-            }
+
+        };
+
             if(count_min == arr.length){
                 obj.min = arr[i]
             }
+
             if(count_max == arr.length){
                 obj.max = arr[i]
             }
+
             count_max = 0;
             count_min =0;
-        }
-        return obj
-    }
 
-    console.log(maxAndMin([1,2,-6,4,5,2,0,10]))
+    }
+        return obj
+};
+
+console.log(maxAndMin([1,2,-6,4,5,2,0,10]))
--- task3.js
+++ task3.js
@@ -1,17 +1,23 @@
 function checkSort(arr){
     let count = 0;
+
     for(let i = 0; i<arr.length-1; i++){
+
         for (let j=i+1; j<arr.length; j++){
+
             if(arr[i] <= arr[j]){
                 count++;
             }
-        }
+
+        };
+
         if(count != arr.length-1){
             return false;
         }
+        
         count = i+1;
-    }
+    };
     return true;
-}
+};
 
 console.log(checkSort([-2,0,0,1,2,3,3,5]))
